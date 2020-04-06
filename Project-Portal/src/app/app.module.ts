04f6e10609c2809c872308914b682be24b0992f9
Xuzijian <zix10@pitt.edu>
2020-04-06 18:44:51 -0400

    All APIs in Postgres update Portal

--- Project-Portal/src/app/app.module.ts
+++ Project-Portal/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import {UsMapModule} from 'angular-us-map'
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { HomepageComponent } from './homepage/homepage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsMapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
